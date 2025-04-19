2248f8a748b5afefc0d3fdd89644f2683508b299
Sadab14 <sm.sadab.shakib@g.bracu.ac.bd>
2025-04-19 21:20:32 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    df = pd.read_excel('data/stock.xlsx')
+    products = df.to_dict(orient='records')  # This includes the new Category column
+    return render_template('index.html', products=products)
+
+@app.route('/invoice')
+def invoice():
+    df = pd.read_excel('data/stock.xlsx')
+    products = df[df['Stock'] > 0].to_dict(orient='records')  # Only products with stock > 0
+    return render_template('invoice.html', products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True)
